30c37aa9d912e6e0b0c4fd0ec968a3bc58afe9e0
gperez <git@github.com:Untitled1Unmastered1>
2020-04-15 00:24:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,8 +24,8 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)
+	  @post = Post.find(params[:id]) #find my post 
+	  @post.update(params.require(:post).permit(:title, :description)) #update my post into the params, require allows us to nest hash into post hash
+	  redirect_to post_path(@post) #redirect to show page 
 	end
 end
